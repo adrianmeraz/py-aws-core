b0d91e3828ac2f74479b8da2f7c4449ca0743bd3
Adrian <adrian@rydeas.com>
2024-06-24 18:15:43 -0500

    added boto3 decorator

--- py_aws_core/decorators.py
+++ py_aws_core/decorators.py
@@ -9,6 +9,26 @@ from . import dynamodb
 logger = logging.getLogger(__name__)
 
 
+def boto3_handler(raise_as, client_error_map: dict):
+    def deco_func(func):
+        @wraps(func)
+        def wrapper_func(*args, **kwargs):
+            try:
+                response = func(*args, **kwargs)
+                logger.debug(f'{__name__}, response: {response}')
+                return response
+            except ClientError as e:
+                error_code = e.response['Error']['Code']
+                logger.error(f'boto3 client error response: {e.response}, error code: {error_code}')
+                if exc := client_error_map.get(error_code):
+                    raise exc(e)
+                raise raise_as(str(e), error=error_code)
+            except Exception:  # Raise all other exceptions as is
+                raise
+        return wrapper_func  # true decorator
+    return deco_func
+
+
 def dynamodb_handler(client_err_map: Dict[str, Any], cancellation_err_maps: List[Dict[str, Any]]):
     def deco_func(func):
         @wraps(func)
