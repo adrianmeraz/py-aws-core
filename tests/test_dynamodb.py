5c95206663e7de5743c9b2d804d63e4ca6315a9d
Adrian <adrian@rydeas.com>
2024-07-13 11:08:41 -0500

    added calc_expire_at_timestamp tests

--- tests/test_dynamodb.py
+++ tests/test_dynamodb.py
@@ -1,8 +1,19 @@
+import datetime
 from unittest import mock, TestCase
-from py_aws_core import dynamodb
+
+from py_aws_core import dynamodb, utils
+
 
 class ABCCommonAPITests(TestCase):
-    def test_calc_expire_at_timestamp(self):
-        val = dynamodb.ABCCommonAPI.calc_expire_at_timestamp()
+    @mock.patch.object(utils, 'get_now_timestamp')
+    def test_calc_expire_at_timestamp(self, mocked_get_now_timestamp):
         dt = datetime.datetime(year=2003, month=9, day=5, hour=15, minute=33, second=28, tzinfo=datetime.timezone.utc)
         mocked_get_now_timestamp.return_value = dt
+        val = dynamodb.ABCCommonAPI.calc_expire_at_timestamp(days=1)
+        self.assertEqual(val, 1062862408)
+
+        val = dynamodb.ABCCommonAPI.calc_expire_at_timestamp(days=180)
+        self.assertEqual(val, 1078328008)
+
+        val = dynamodb.ABCCommonAPI.calc_expire_at_timestamp(days=None)
+        self.assertEqual(val, '')
