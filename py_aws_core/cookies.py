f488a0d0afa798b8d323958b63d81dd3f092f4f2
Adrian <adrian@rydeas.com>
2024-07-21 22:22:16 -0500

    added cookie util for generating headers

--- py_aws_core/cookies.py
+++ py_aws_core/cookies.py
@@ -1,50 +1,26 @@
-from http.cookiejar import Cookie
 from http.cookies import SimpleCookie
-
-import datetime
 
 from . import utils
 
 
-def build_set_cookie_header_value(
-    name: str,
-    domain: str,
-    value: str,
-    path: str,
-    expires: int
-) -> str:
-    return str(Cookie(
-        version=None,
-        name=name,
-        value=value,
-        port=None,
-        port_specified=False,
-        domain=domain,
-        domain_specified=True,
-        domain_initial_dot=domain.startswith('.'),
-        path=path,
-        path_specified=path is not None,
-        secure=True,
-        expires=expires,
-        discard=False,
-        comment=None,
-        comment_url=None,
-        rest=dict(),
-        rfc2109=False,
-    ))
+class CookieUtil:
+    @classmethod
+    def build_set_cookie_header(
+        cls,
+        name: str,
+        domain: str,
+        value: str,
+        path: str,
+        expires_in_seconds: int
+    ) -> str:
+        cookie = SimpleCookie()
+        cookie[name] = value
+        cookie[name]['domain'] = domain
+        cookie[name]['path'] = path
+        cookie[name]['expires'] = cls.get_expiry_timestamp(expires_in_seconds)
+        return cookie.output(header="Set-Cookie:", sep="\015\012")
 
-
-def build_set_cookie_header_value_2(
-    name: str,
-    domain: str,
-    value: str,
-    path: str,
-    expires_in_seconds: int
-) -> str:
-    cookie = SimpleCookie()
-    cookie[name] = value
-    cookie[name]['domain'] = domain
-    cookie[name]['path'] = path
-    expires = utils.add_seconds_to_now_datetime
-    # cookie['session']['expires'] = expires.strftime("%a, %d %b %Y %H:%M:%S GMT")
-    return cookie.output(header="Set-Cookie:", sep="\015\012")
+    @classmethod
+    def get_expiry_timestamp(cls, expire_in_seconds: int) -> str:
+        dt = utils.add_seconds_to_now_datetime(seconds=expire_in_seconds)
+        return dt.strftime("%a, %d %b %Y %H:%M:%S GMT")
