24a83f6e7f22d6852ec8bfbdfec554de1ad7fb02
Adrian <adrian@rydeas.com>
2024-07-24 09:29:39 -0500

    refactored test

--- tests/test_clients.py
+++ tests/test_clients.py
@@ -11,7 +11,7 @@ class RetryClientTests(TestCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.request = Request(url='', method='')  # Must add a non null request to avoid raising Runtime exception
-        cls.mocked_sleep = mock.patch('time.sleep', return_value=None).start()
+        cls.mocked_sleep = mock.patch('py_aws_core.utils.sleep', return_value=None).start()
 
     @mock.patch.object(RetryClient, '_send_handling_auth')
     def test_ok(self, mock_send):
@@ -20,12 +20,12 @@ class RetryClientTests(TestCase):
             status_code=codes.OK,
             text='ok'
         )
-        with RetryClient() as h_client:
-            h_client.get('http://example.com')
+        with RetryClient() as retry_client:
+            retry_client.get('http://example.com')
             self.assertEqual(mock_send.call_count, 1)
 
     @mock.patch.object(RetryClient, '_send_handling_auth')
-    def test_retry(self, mock_send):
+    def test_retryable_exception(self, mock_send):
         mock_send.side_effect = NetworkError(message='')
         with self.assertRaises(NetworkError):
             with RetryClient() as h_client:
@@ -33,10 +33,10 @@ class RetryClientTests(TestCase):
         self.assertEqual(mock_send.call_count, 4)
 
     @mock.patch.object(RetryClient, '_send_handling_auth')
-    def test_raise_api_error(self, mock_send):
+    def test_non_retryable_http_status_code(self, mock_send):
         response = Response(
             request=self.request,
-            status_code=codes.NOT_FOUND,
+            status_code=404,  # Non-retryable
         )
         mock_send.side_effect = HTTPStatusError(message='test_123', request=self.request, response=response)
         with self.assertRaises(APIException):
