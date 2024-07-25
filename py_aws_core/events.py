3668278b4a2075d99ba5860932c8c90cc6e5eb2e
Adrian <adrian@rydeas.com>
2024-07-25 10:52:48 -0500

    added multi value properties tests

--- py_aws_core/events.py
+++ py_aws_core/events.py
@@ -3,30 +3,52 @@ import typing
 
 
 class LambdaEvent:
+    class MultiValueHeaders:
+        def __init__(self, data: dict):
+            self.accept = data['Accept'][0]
+            self.accept_encoding = data['Accept-Encoding'][0]
+            self._authorization = data.get('Authorization')
+            self._cookies = data.get('Cookie')
+            self.user_agent = data['User-Agent'][0]
+
+        @property
+        def authorization(self):
+            if self._authorization:
+                return self._authorization[0]
+            return None
+
+        @property
+        def cookies(self):
+            return self._cookies or list()
+
     class RequestContext:
         def __init__(self, data):
-            self.resourceId = data['resourceId']
-            self.resourcePath = data['resourcePath']
-            self.httpMethod = data['httpMethod']
-            self.requestTime = data['requestTime']
+            self.resource_id = data['resourceId']
+            self.resource_path = data['resourcePath']
+            self.http_method = data['httpMethod']
+            self.request_time = data['requestTime']
             self.path = data['path']
-            self.domainName = data['domainName']
+            self.domain_name = data['domainName']
 
     def __init__(self, data):
         self._body = data['body']
         self.headers = data['headers'] or dict()
-        self.httpMethod = data['httpMethod']
-        self.multiValueHeaders = data['multiValueHeaders']
-        self.multiValueQueryStringParameters = data['multiValueQueryStringParameters']
+        self.http_method = data['httpMethod']
+        self.multi_value_headers = self.MultiValueHeaders(data['multiValueHeaders'])
+        self.multi_value_query_string_parameters = data['multiValueQueryStringParameters']
         self.path = data['path']
-        self.queryStringParameters = data['queryStringParameters'] or dict()
-        self.requestContext = self.RequestContext(data['requestContext'])
+        self.query_string_parameters = data['queryStringParameters'] or dict()
+        self.request_context = self.RequestContext(data['requestContext'])
 
     @property
     def body(self):
         if self._body:
             return json.loads(self._body)
         return self._body
+
+    @property
+    def cookies(self):
+        return self.multi_value_headers._cookies
 
     @property
     def lower_headers(self) -> typing.Dict:
