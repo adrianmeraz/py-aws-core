9dc468368a709574c9a00fefb1e56f2e3e78960f
Adrian <adrian@rydeas.com>
2024-08-01 14:14:18 -0500

    added client cookie tests

--- tests/test_clients.py
+++ tests/test_clients.py
@@ -3,7 +3,7 @@ from unittest import mock
 
 from httpx import HTTPStatusError, NetworkError, Request, Response, codes
 
-from py_aws_core import services
+from py_aws_core import exceptions, services
 from py_aws_core.clients import RetryClient, SessionPersistClient
 from py_aws_core.exceptions import APIException
 from py_aws_core.testing import BaseTestFixture
@@ -47,7 +47,7 @@ class RetryClientTests(BaseTestFixture):
                 h_client.get('https://example.com')
         self.assertEqual(mock_send.call_count, 1)
 
-    def test_cookies(self):
+    def test_ok_cookies(self):
         client = RetryClient()
         self.assertEqual(len(client.cookies), 0)
 
@@ -69,10 +69,6 @@ class RetryClientTests(BaseTestFixture):
         client = RetryClient()
         self.assertEqual(len(client.cookies), 0)
 
-        # Verify no cookies results in empty cookie jar
-        client.b64_decode_and_set_cookies(b'')
-        self.assertEqual(len(client.cookies), 0)
-
         # Now rehydrate cookies from binary
         client.b64_decode_and_set_cookies(
             b'gASVjAEAAAAAAABdlCiMDmh0dHAuY29va2llamFylIwGQ29va2lllJOUKYGUfZQojAd2ZXJzaW9u\nlEsBjARuYW1llIwIY29va2llXzGUjAV2YWx1ZZSMB3ZhbHVlXzGUjARwb3J0lE6MDnBvcnRfc3Bl\nY2lmaWVklImMBmRvbWFpbpSMD3d3dy5leGFtcGxlLmNvbZSMEGRvbWFpbl9zcGVjaWZpZWSUiIwS\nZG9tYWluX2luaXRpYWxfZG90lImMBHBhdGiUjAEvlIwOcGF0aF9zcGVjaWZpZWSUiIwGc2VjdXJl\nlIiMB2V4cGlyZXOUTRAOjAdkaXNjYXJklImMB2NvbW1lbnSUTowLY29tbWVudF91cmyUTowHcmZj\nMjEwOZSJjAVfcmVzdJR9lHViaAMpgZR9lChoBksBaAeMCGNvb2tpZV8ylGgJjAd2YWx1ZV8ylGgL\nTmgMiWgNjA93d3cuZXhhbXBsZS5jb22UaA+IaBCJaBFoEmgTiGgUiGgVTRAOaBaJaBdOaBhOaBmJ\naBp9lHViZS4=\n')
@@ -87,6 +83,16 @@ class RetryClientTests(BaseTestFixture):
         r_cookie_2 = r_cookies['cookie_2']
         self.assertEqual(r_cookie_2.name, 'cookie_2')
         self.assertEqual(r_cookie_2.value, 'value_2')
+
+    def test_cookie_errors(self):
+        client = RetryClient()
+
+        # Verify no cookies results in empty cookie jar
+        client.b64_decode_and_set_cookies(b'')
+        self.assertEqual(len(client.cookies), 0)
+
+        with self.assertRaises(exceptions.CookieDecodingError):
+            client.b64_decode_and_set_cookies(b'badcookies')
 
 
 class SessionPersistClientTests(BaseTestFixture):
