40a9f66ecb5ea3b8811c4d744297f01eb52d29d2
Adrian <adrian@rydeas.com>
2024-08-31 12:53:09 -0500

    added test for empty get item responses

--- tests/test_encoders.py
+++ tests/test_encoders.py
@@ -9,7 +9,7 @@ from tests import const as test_const
 
 class DBEncoderTests(TestCase):
     def test_serialize_to_json(self):
-        source = test_const.TEST_DB_RESOURCES_PATH.joinpath('get_items.json')
+        source = test_const.TEST_DB_RESOURCES_PATH.joinpath('db#get_items.json')
         with as_file(source) as entities_json:
             to_json = encoders.DBEncoder().serialize_to_json(json.loads(entities_json.read_text(encoding='utf-8')))
             self.assertEqual(to_json, '[{"disabled":false,"group":null,"selected":false,"text":"BOGOTA D.C (BOG), AEROPUERTO EL DORADO","value":"1"},{"disabled":false,"group":null,"selected":false,"text":"CARTAGENA (CTG), AEROPUERTO RAFAEL NUÑEZ","value":"18"},{"disabled":false,"group":null,"selected":false,"text":"MEDELLIN (MDE), AEROPUERTO JOSE MARIA CORDOVA","value":"12"},{"disabled":false,"group":null,"selected":false,"text":"PEREIRA (PEI), AEROPUERTO MATECAÑA","value":"15"},{"disabled":false,"group":null,"selected":false,"text":"RIOHACHA (RCH), AEROPUERTO ALMIRANTE PADILLA","value":"76"},{"disabled":false,"group":null,"selected":false,"text":"SAN ANDRES (ADZ) AEROPUERTO GUSTAVO ROJAS PINILLA","value":"16"}]')
