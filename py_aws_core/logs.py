9fbd31aff2ad1130a6e6371c37a2695f03fac319
Adrian <adrian@rydeas.com>
2024-10-04 14:35:41 -0500

    cleaned up log formatters

--- py_aws_core/logs.py
+++ py_aws_core/logs.py
@@ -1,21 +1,38 @@
 import logging
 import os
+import sys
 
 import structlog
 from dotenv import load_dotenv
 
-
 dotenv = load_dotenv()  # take environment variables from .env.
 level = os.environ.get("LOG_LEVEL", "INFO").upper()
 LOG_LEVEL = getattr(logging, level)
-structlog.configure(
-    processors=[
-        structlog.processors.EventRenamer("message"),
-        structlog.processors.CallsiteParameterAdder(),
-        structlog.processors.TimeStamper(fmt="iso"),
-        structlog.processors.add_log_level,
+
+shared_processors = [
+    structlog.processors.EventRenamer('message'),
+    structlog.processors.CallsiteParameterAdder(),
+    structlog.processors.TimeStamper(fmt="iso"),
+    structlog.processors.add_log_level,
+]
+val = sys.stderr.isatty()
+if 'unittest' in sys.modules or sys.stderr.isatty():
+    # Pretty printing when we run tests or a terminal session.
+    # Automatically prints pretty tracebacks when "rich" is installed
+    processors = shared_processors + [
+        structlog.dev.ConsoleRenderer(
+            colors=True,
+        ),
+    ]
+else:
+    # Print JSON when we run, e.g., in a Docker container.
+    # Also print structured tracebacks.
+    processors = shared_processors + [
+        structlog.processors.dict_tracebacks,
         structlog.processors.JSONRenderer(),
-    ],
+    ]
+structlog.configure(
+    processors=processors,
     wrapper_class=structlog.make_filtering_bound_logger(LOG_LEVEL)
 )
 
