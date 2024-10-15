5d414107cd2957196219c885ecd6b0e6df23858f
Adrian <adrian@rydeas.com>
2024-10-15 15:49:35 -0500

    refactored for dependency injection

--- py_aws_core/ssm_parameter_store.py
+++ py_aws_core/ssm_parameter_store.py
@@ -1,6 +1,6 @@
 import json
 
-import boto3
+from botocore.client import BaseClient
 from botocore.exceptions import ClientError
 
 from . import exceptions, logs, utils
@@ -9,7 +9,11 @@ logger = logs.get_logger()
 
 
 class SSMParameterStore:
-
+    """
+        First checks environment variables for secrets.
+        Second, checks cached secrets
+        Third, if secret not found, will attempt to pull from secrets manager
+    """
     class Response:
         class Parameter:
             def __init__(self, data):
@@ -30,28 +34,24 @@ class SSMParameterStore:
         def parameter_json(self):
             return json.loads(self.Parameter.Value)
 
-    """
-    First checks environment variables for secrets.
-    If secret not found, will attempt to pull from secrets manager
-    """
-    AWS_SECRET_NAME_KEY = 'AWS_SECRET_NAME'
+    AWS_SECRET_ID_KEY = 'AWS_SECRET_NAME'
 
-    def __init__(self):
-        self._boto_client = None
-        self._secrets_map = dict()
+    def __init__(self, boto_client: BaseClient):
+        self._boto_client = boto_client
+        self._cached_secrets_map = dict()
 
     def get_secret(self, secret_name: str):
         if secret_value := utils.get_environment_variable(secret_name):
             logger.debug(f'Secret "{secret_name}" found in environment variables')
             return secret_value
-        if val := self._secrets_map.get(secret_name):
+        if val := self._cached_secrets_map.get(secret_name):
             logger.debug(f'Secret "{secret_name}" found in cached secrets')
             return val
         try:
-            r_get_parameter = self.boto_client.get_parameter(Name=self.get_aws_secret_name)
+            r_get_parameter = self._boto_client.get_parameter(Name=self.aws_secret_id)
             r_get_parameter = self.Response(r_get_parameter)
-            self._secrets_map = r_get_parameter.parameter_json
-            return self._secrets_map[secret_name]
+            self._cached_secrets_map = r_get_parameter.parameter_json
+            return self._cached_secrets_map[secret_name]
         except ClientError as e:
             logger.exception(f'Error while trying to find secret "{secret_name}"')
             # For a list of exceptions thrown, see
@@ -59,27 +59,7 @@ class SSMParameterStore:
             raise exceptions.SecretsManagerException(e)
 
     @property
-    def boto_client(self):
-        if not self._boto_client:
-            self._boto_client = boto3.client('ssm')
-        return self._boto_client
-
-    @boto_client.setter
-    def boto_client(self, value):
-        self._boto_client = value
-
-    def get_aws_secret_name(self) -> str:
-        if aws_secret_id := utils.get_environment_variable(self.AWS_SECRET_NAME_KEY):
+    def aws_secret_id(self) -> str:
+        if aws_secret_id := utils.get_environment_variable(self.AWS_SECRET_ID_KEY):
             return aws_secret_id
-        raise exceptions.SecretsManagerException(f'Missing environment variable "{self.AWS_SECRET_NAME_KEY}"')
-
-
-__secrets_manager = SSMParameterStore()
-
-
-def get_secrets_manager() -> SSMParameterStore:
-    """
-    Reuses secrets manager across all modules for efficiency
-    :return:
-    """
-    return __secrets_manager
+        raise exceptions.SecretsManagerException(f'Missing environment variable "{self.AWS_SECRET_ID_KEY}"')
--- tests/test_ssm_parameter_store.py
+++ tests/test_ssm_parameter_store.py
@@ -3,6 +3,7 @@ from importlib.resources import as_file
 from unittest import mock, TestCase
 from unittest.mock import PropertyMock
 
+import boto3
 from botocore.stub import Stubber
 
 from py_aws_core import utils
@@ -14,19 +15,24 @@ class SSMParameterStoreTests(TestCase):
     @mock.patch.object(utils, 'get_environment_variable')
     def test_get_secret_env_var(self, mocked_get_env_var):
         mocked_get_env_var.return_value = 'TEST_VAL_1'
-        sm = SSMParameterStore()
-        stubber = Stubber(sm.boto_client)
+        boto_client = boto3.client('ssm')
+
+        stubber = Stubber(boto_client)
         stubber.activate()
+
+        sm = SSMParameterStore(boto_client=boto_client)
         val = sm.get_secret(secret_name='TEST_KEY_1')
         self.assertEqual('TEST_VAL_1', val)
 
-    @mock.patch.object(SSMParameterStore, new_callable=PropertyMock, attribute='get_aws_secret_name')
-    def test_get_secret_caching(self, mock_get_aws_secret_name):
-        mock_get_aws_secret_name.return_value = 'TEST_VAL_2'
+    @mock.patch.object(SSMParameterStore, new_callable=PropertyMock, attribute='aws_secret_id')
+    def test_get_secret_caching(self, mocked_aws_secret_id):
+        mocked_aws_secret_id.return_value = 'TEST_VAL_2'
+        boto_client = boto3.client('ssm')
 
-        ssm = SSMParameterStore()
-        stubber = Stubber(ssm.boto_client)
+        stubber = Stubber(boto_client)
         stubber.activate()
+
+        ssm = SSMParameterStore(boto_client=boto_client)
         source = test_const.TEST_SSM_RESOURCES_PATH.joinpath('get_parameter.json')
         with as_file(source) as parameter_json:
             stubber.add_response('get_parameter', json.loads(parameter_json.read_text(encoding='utf-8')))
