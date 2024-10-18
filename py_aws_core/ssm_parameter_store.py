3e50e5c4084e8473b8e476028143cae058fb8024
Adrian <adrian@rydeas.com>
2024-10-18 10:12:57 -0500

    added param to pass cached secrets

--- py_aws_core/ssm_parameter_store.py
+++ py_aws_core/ssm_parameter_store.py
@@ -36,22 +36,22 @@ class SSMParameterStore:
 
     AWS_SECRET_ID_KEY = 'AWS_SECRET_NAME'
 
-    def __init__(self, boto_client: BaseClient):
+    def __init__(self, boto_client: BaseClient, cached_secrets: dict = None):
         self._boto_client = boto_client
-        self._cached_secrets_map = dict()
+        self._cached_secrets = cached_secrets or dict()
 
     def get_secret(self, secret_name: str):
         if secret_value := utils.get_environment_variable(secret_name):
             logger.debug(f'Secret "{secret_name}" found in environment variables')
             return secret_value
-        if val := self._cached_secrets_map.get(secret_name):
+        if val := self._cached_secrets.get(secret_name):
             logger.debug(f'Secret "{secret_name}" found in cached secrets')
             return val
         try:
             r_get_parameter = self._boto_client.get_parameter(Name=self.aws_secret_id)
             r_get_parameter = self.Response(r_get_parameter)
-            self._cached_secrets_map = r_get_parameter.parameter_json
-            return self._cached_secrets_map[secret_name]
+            self._cached_secrets = r_get_parameter.parameter_json
+            return self._cached_secrets[secret_name]
         except ClientError as e:
             logger.exception(f'Error while trying to find secret "{secret_name}"')
             # For a list of exceptions thrown, see
